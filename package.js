c57a3b333b37e65a0360ff019c5d64ab81078f54
Dennis Keller <dennis@gozio.me>
2014-12-02 13:48:19 -0800

    Version contraint

--- package.js
+++ package.js
@@ -6,7 +6,7 @@ Package.describe({
 });
 
 Package.on_use(function(api) {
+  api.versonsFrom('0.9.0');
   api.use('jquery', 'client');
-
   api.add_files('jquery.inputmask/dist/jquery.inputmask.bundle.js', 'client');
 });
