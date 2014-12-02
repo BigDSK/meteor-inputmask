3eba1f5e2df02b8c3f05f97f65c4dd7c2fdc0b1f
Dennis Keller <dennis@gozio.me>
2014-12-02 13:37:59 -0800

    Meteor inputmask package

---
+++ README.md
@@ -0,0 +1,7 @@
+## Synopsis
+
+A simple packaging of [jquery.inputmask](https://github.com/RobinHerbots/jquery.inputmask) for Meteor.
+
+## Installation
+
+	meteor add bigdsk:inputmask
---
+++ package.js
@@ -0,0 +1,12 @@
+Package.describe({
+  "summary": "JQuery InputMask for Meteor",
+  "version": "3.1.42",
+  "git": "https://github.com/BigDSK/meteor-inputmask.git",
+  "name": "bigdsk:inputmask"
+});
+
+Package.on_use(function(api) {
+  api.use('jquery', 'client');
+
+  api.add_files('jquery.inputmask/dist/jquery.inputmask.bundle.js', 'client');
+});
