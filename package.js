b389f409a7207716f8f9ce6a72f0b7eb45f1e02a
Dennis Keller <dennis@gozio.me>
2016-10-10 09:54:36 -0700

    Update to version 3.3.3

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   "summary": "JQuery InputMask for Meteor",
-  "version": "3.1.63",
+  "version": "3.3.3",
   "git": "https://github.com/BigDSK/meteor-inputmask.git",
   "name": "bigdsk:inputmask"
 });
